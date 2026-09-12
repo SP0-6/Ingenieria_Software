7dc644c0558ca8f563554e073a011eec1f5e8d26
LP05 <spgit0507@gmail.com>
2026-09-12 17:01:12 -0300

    abm 2 tp3

---
+++ Unidad2/tp3-individual/ABMs/ABM-2/src/EmployeesData.js
@@ -0,0 +1,20 @@
+export const Data = [
+  {
+    id: 1,
+    firstName: "Sofia",
+    lastName: "Parra",
+    age: 20,
+  },
+  {
+    id: 2,
+    firstName: "Chiara",
+    lastName: "Bernhard",
+    age: 34,
+  },
+  {
+    id: 3,
+    firstName: "Graciana",
+    lastName: "March",
+    age: 35,
+  },
+];
